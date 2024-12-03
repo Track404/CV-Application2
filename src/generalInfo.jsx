9f2e9034f103d4370e375d47bc332725d79a21b1
Track404 <111713000+Track404@users.noreply.github.com>
2024-12-03 17:07:27 +0100

    general info component grab input into a state

--- src/generalInfo.jsx
+++ src/generalInfo.jsx
@@ -1,32 +1,37 @@
-
+import { useState } from 'react';
 import './App.css'
 
-function generalInfo() {
-  
+function GeneralInfo() {
+
+  const [generalInformation, setGeneralInformation] = useState({name:"",email:"",tel:""});
+  function handleSubmit(e) {
+    e.preventDefault();
+      alert(`You said your name is${generalInformation.name}, your email is ${generalInformation.email} and your phone number is ${generalInformation.tel} `)
+  }
 
   return (
     <>
       <div className='generalInfo'>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label >
             Name:
-            <input type="text" name="name" id="name" />
+            <input type="text" name="name" id="name" value={generalInformation.name} onChange={e => setGeneralInformation({...generalInformation,name:e.target.value})}/>
           </label>
           
           <label >
             Email:
-            <input type="email" name="email" id="email" />
+            <input type="email" name="email" id="email" value={generalInformation.email} onChange={e => setGeneralInformation({...generalInformation,email:e.target.value})}/>
           </label>
           
           <label >
             Phone Number:
-            <input type="tel" name="phone" id="phone" />
+            <input type="tel" name="phone" id="phone" value={generalInformation.tel} onChange={e => setGeneralInformation({...generalInformation,tel:e.target.value})}/>
           </label>
-          
+          <button type="submit">Send</button>
         </form>
       </div>
     </>
   )
 }
 
-export default generalInfo
+export default GeneralInfo
